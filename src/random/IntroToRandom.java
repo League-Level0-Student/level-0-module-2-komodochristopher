a351ca59ecd78144aef8229c6cf22dcce76f6640
Dong Zou <jiwenzou@gmail.com>
2018-10-27 17:26:12 -0700

    random intro

--- src/random/IntroToRandom.java
+++ src/random/IntroToRandom.java
@@ -8,14 +8,15 @@ import java.util.Random;
 public class IntroToRandom {
 	public static void main(String[] args) {
 		
-		int r = 0;
+		
 		
 		//3. Now make r random. 
 		//   Create an object of the Random class
 		//	 use .nextInt() to get a random number
-		
+		Random arg=new Random();
 
 		//4. Limit the random number between 0 and 100
+		int randomChoice=arg.nextInt(101);
 
 		//5. Limit the random number between 25 and 75. Hint: ((highest value - lowest value)+1) + lowest value
 
